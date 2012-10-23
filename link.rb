f7c6c04a2d457767d8faccdaefadbed97849417f
Jordan Evans <jordan.evans@rackspace.com>
2012-10-23 14:24:55 -0700

    update link script

--- link.rb
+++ link.rb
@@ -1,20 +1,23 @@
 #!/usr/bin/ruby -w
 
-home = ENV['HOME']
-cwd = Dir.pwd
+if __FILE__ == $0
+  home = ENV['HOME']
+  cwd = Dir.pwd
 
-files = {
-  :Xmodmap => "#{home}/.Xmodmap",
-  :config  => "#{home}/.config",
-  :screenrc => "#{home}/.screenrc",
-  :ssh_config => "#{home}/.ssh/config",
-  :xinitrc => "#{home}/.xinitrc",
-  :xmodmap_modkeys => "#{home}/.xmodmap-modkeys",
-  :zshrc => "#{home}/.zshrc",
-  :zshrc_d => "#{home}/.zshrc.d",
-}
+  files = {
+    :Xmodmap => "#{home}/.Xmodmap",
+    :config  => "#{home}/.config",
+    :screenrc => "#{home}/.screenrc",
+    :ssh_config => "#{home}/.ssh/config",
+    :xinitrc => "#{home}/.xinitrc",
+    :xmodmap_modkeys => "#{home}/.xmodmap-modkeys",
+    :zshrc => "#{home}/.zshrc",
+    :zshrc_d => "#{home}/.zshrc.d",
+    :dne => "#{home}/.dne" # Does not exist, test case
+  }
 
-files.each_pair do |k,v|
-#  puts "ln -s #{k} #{v}"
-  `ln -sf #{cwd}/#{k} #{v}`
+  files.each_pair do |k,v|
+    File.unlink(v) if File.exists?(v) and File.ftype(v) == 'link'
+    File.symlink("#{cwd}/#{k}",v) if !File.exists?(v)
+  end
 end
